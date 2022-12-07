79a199ff36d3e5c362bbbcb98a2ab0b6165c7996
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-07 14:44:18 +0100

    Initial Commit

---
+++ TodoListSwiftful/TodoListSwiftfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftfulApp.swift
+//  TodoListSwiftful
+//
+//  Created by Kaori Persson on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
