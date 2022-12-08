cc052d8b3aeb4481512dd53718c04ea8b3f1a60d
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-08 09:44:17 +0100

    Style addView

--- TodoListSwiftful/Views/AddView.swift
+++ TodoListSwiftful/Views/AddView.swift
@@ -10,24 +10,37 @@ import SwiftUI
 struct AddView: View {
   
   @State var textFieldText: String = ""
-    var body: some View {
-      ScrollView {
-        VStack {
-          TextField("Type something here...", text: $textFieldText)
-            .frame(height: 55)
-            .background(Color(.systemGray6))
+  var body: some View {
+    ScrollView {
+      VStack {
+        TextField("Type something here...", text: $textFieldText)
+          .frame(height: 55)
+          .background(Color(.systemGray6))
           .cornerRadius(10)
+        
+        Button {
+          
+        } label: {
+          Text("Save".uppercased())
+            .foregroundColor(.white)
+            .font(.headline)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(Color.accentColor)
+            .cornerRadius(10)
         }
-        .padding(14)
+
       }
-      .navigationTitle("Add an Item  🖊️")
+      .padding(14)
     }
+    .navigationTitle("Add an Item  🖊️")
+  }
 }
 
 struct AddView_Previews: PreviewProvider {
-    static var previews: some View {
-      NavigationView {
-        AddView()
-      }
+  static var previews: some View {
+    NavigationView {
+      AddView()
     }
+  }
 }
